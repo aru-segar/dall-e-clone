4450af9a10a790f453ca04fd00b694d595b44af7
aru_segar <arunigagnanasegaran@gmail.com>
2025-03-26 22:19:40 +0530

    implemented the structure for creating routes

--- backend/index.js
+++ backend/index.js
@@ -1,13 +1,18 @@
 import express from "express"; // import tools
 import * as dotenv from "dotenv";
 import cors from "cors";
-import connectDB from "./mongodb/connect";
+import connectDB from "./mongodb/connect.js";
+import postRoutes from "./routes/postRoutes.js";
+import dalleRoutes from "./routes/dalleRoutes.js";
 
 dotenv.config(); // load .env file
 
 const app = express(); // create express app
 app.use(cors()); // enable CORS
 app.use(express.json({ limit: "50mb" })); // enable JSON body parsing
+
+app.use("/api/v1/post", postRoutes); // use post routes
+app.use("/api/v1/dalle", dalleRoutes); // use dalle routes
 
 app.get("/", async (req, res) => {
   // define a route handler for the default home page
