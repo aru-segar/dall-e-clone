5b8d43e516590484f36da594421ae5e77d6a8773
aru_segar <arunigagnanasegaran@gmail.com>
2025-03-27 20:54:39 +0530

    integrated backend to frontend

--- frontend/src/pages/CreatePost.jsx
+++ frontend/src/pages/CreatePost.jsx
@@ -33,7 +33,7 @@ const CreatePost = () => {
       try {
         setGeneratingImg(true);
         const response = await fetch(
-          "https://localhost:8080/api/v1/dalle",
+          "http://localhost:8080/api/v1/dalle",
           {
             method: "POST",
             headers: {
@@ -65,7 +65,7 @@ const CreatePost = () => {
       setLoading(true);
       try {
         const response = await fetch(
-          "https://dalle-arbb.onrender.com/api/v1/post",
+          "http://localhost:8080/api/v1/post",
           {
             method: "POST",
             headers: {
